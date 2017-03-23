72e1259f975ee1af53a2ff3eb412e0c02b5db7da
Ksenia Annenko <kseniagor.link@gmail.com>
2017-03-23 09:45:12 +0200

    Add gamePreset object with defoult players array, set and get players method. Task: The ViewModel part should provide an API #5

--- source/view-model.js
+++ source/view-model.js
@@ -4,14 +4,54 @@
 
 /*********************** View-Model **********************/
 "use strict";
+function GameInfo(game){
+    this.getFieldCells = function(){
+        return game.getFieldCells();
+    };
+
+    this.getCurrentStatus = function(){
+        let gameStatus = game.currentStatus;
+        let playerName = gameStatus == 'Tie' ? 'It' : game.getCurrentPlayerName();
+        return  playerName + ' is ' + gameStatus;
+    };
+};
+
+let gamePreset = {
+    players: [
+        {playerId : '01', type : 'human', name : 'First', symbol : 'X'},
+        {playerId : '02', type : 'computer', name : 'Computer', symbol : 'O'}
+    ],
+
+    setPlayer: function(id, key, value){
+        this.players.forEach(function(item){
+            if(item.playerId == id){
+                for(let i in item){
+                    if(i == key && item.hasOwnProperty(i)){
+                        item[i] = value;
+                        break;
+                    }
+                }
+            }
+        });
+        return this.players;
+    },
+
+    getPlayer: function(){
+        return this.players;
+    }
+};
+
+
 function ViewModel(){
     this.game = null;
 
     this.getPlayersArray = function () {
         let array = [];
         let factory = new Factory();
-        array.push(factory.createPlayer('human', 'First', 'X'));
-        array.push(factory.createPlayer('computer', 'Computer', 'O'));
+
+        gamePreset.getPlayers.forEach(function(item){
+            array.push(factory.createPlayer(item.type, item.name, item.symbol));
+        });
         return array;
     };
 
@@ -29,14 +69,3 @@ function ViewModel(){
     };
 };
 
-function GameInfo(game){
-    this.getFieldCells = function(){
-        return game.getFieldCells();
-    };
-
-    this.getCurrentStatus = function(){
-        let gameStatus = game.currentStatus;
-        let playerName = gameStatus == 'Tie' ? 'It' : game.getCurrentPlayerName();
-        return  playerName + ' is ' + gameStatus;
-    };
-};

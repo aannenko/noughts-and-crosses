c98aa6c4df72c09f0d63ba4805a58a55ad96b31a
Ksenia Annenko <triniti86@gmail.com>
2017-03-26 13:10:21 +0300

    The view mode part should provide an api#5 (#10)
    * Add gamePreset object with default players array and find, add, remove and update methods.
    
    * Add License.md.
    
    * Add addPlayer and deletePlayer to gamePreset object. Task: The ViewModel part should provide an API#5
    
    * Remove playersId. Task: The ViewModel part should provide an API#5
    
    * Simplify gamePreset methods. Task: The view mode part should provide an api#5 #10
    
    * Change human player name from First to Player.

--- source/view-model.js
+++ source/view-model.js
@@ -4,14 +4,63 @@
 
 /*********************** View-Model **********************/
 "use strict";
+function GameInfo(game){
+    this.getFieldCells = function(){
+        return game.getFieldCells();
+    };
+
+    this.getCurrentStatus = function(){
+        let gameStatus = game.currentStatus;
+        let playerName = gameStatus == 'Tie' ? 'It' : game.getCurrentPlayerName();
+        return  playerName + ' is ' + gameStatus;
+    };
+};
+
+let gamePreset = {
+    players: [
+        {type : 'human', name : 'Player', symbol : 'X'},
+        {type : 'computer', name : 'Computer', symbol : 'O'}
+    ],
+
+    findPlayer: function(playerName){
+        return this.players.find(function(item){
+            return item.name == playerName;
+        });
+    },
+
+    updatePlayer: function(playerName, prop, value){
+        this.findPlayer(playerName)[prop] = value;
+    },
+
+    addPlayer: function(type, name, symbol){
+        let newPlayer = {
+            type: type,
+            name: name,
+            symbol: symbol
+        };
+        this.players.splice(this.players.length, 0, newPlayer);
+    },
+
+    deletePlayer: function(name){
+        for(let i = 0; i < this.players.length; i++){
+            if(this.players[i].name == name){
+                this.players.splice(i, 1);
+            }
+        }
+    }
+};
+
+
 function ViewModel(){
     this.game = null;
 
     this.getPlayersArray = function () {
         let array = [];
         let factory = new Factory();
-        array.push(factory.createPlayer('human', 'First', 'X'));
-        array.push(factory.createPlayer('computer', 'Computer', 'O'));
+
+        gamePreset.players.forEach(function(item){
+            array.push(factory.createPlayer(item.type, item.name, item.symbol));
+        });
         return array;
     };
 
@@ -29,14 +78,3 @@ function ViewModel(){
     };
 };
 
-function GameInfo(game){
-    this.getFieldCells = function(){
-        return game.getFieldCells();
-    };
-
-    this.getCurrentStatus = function(){
-        let gameStatus = game.currentStatus;
-        let playerName = gameStatus == 'Tie' ? 'It' : game.getCurrentPlayerName();
-        return  playerName + ' is ' + gameStatus;
-    };
-};
